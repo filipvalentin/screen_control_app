4e6a643c31435adca6e2df2f4d3f52bc9d019c92
filipvalentin <valentin.filip@student.uaic.ro>
2024-09-09 00:13:00 +0300

    host id is now retrieved and returned

--- ScreenControlApp.Desktop/Common/Settings/ApplicationSettings.cs
+++ ScreenControlApp.Desktop/Common/Settings/ApplicationSettings.cs
@@ -1,6 +1,7 @@
 ﻿namespace ScreenControlApp.Desktop.Common.Settings {
 	public record ApplicationSettings {
 		public string ServerAddress { get; set; } = null!;
+		public string HubName { get; set; } = null!;
 		public int PreferredScreenId { get; set; }
 	}
 }
--- ScreenControlApp.Desktop/MainWindow.xaml.cs
+++ ScreenControlApp.Desktop/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using ScreenControlApp.Desktop.ScreenSharing;
 using System.Windows.Media.Imaging;
 using ScreenControlApp.Desktop.Common.Settings;
 using ScreenControlApp.Desktop.Common;
+using System.Net.Http;
 
 namespace ScreenControlApp.Desktop {
 	/// <summary>
@@ -11,20 +12,32 @@ namespace ScreenControlApp.Desktop {
 	/// </summary>
 	public partial class MainWindow : Window {
 		private ApplicationSettings Settings { get; set; } = null!;
+		private bool IsSettingsPageDisplayed = false;
+		private CancellationTokenSource CancellationTokenSource { get; set; } = new();
 
 		public MainWindow() {
 			InitializeComponent();
 			//Application.Current.MainWindow.WindowState = WindowState.Maximized;
 
-			shareHost_HostId.Text = "123-456-789";
-			shareHost_Passcode.Text = "1234";
-			var a = new QuickControlsWindow();
-			a.Show();
+			SharingSide_Passcode_TextBox.Text = "123";//generate one
+
+			//var a = new QuickControlsWindow();
+			//a.Show();
+			this.Closed += ReturnHostId;
 		}
 
+		private async void ReturnHostId(object? sender, EventArgs e) {
+			using HttpClient client = new();
+			try {
+				_ = await client.DeleteAsync(Settings.ServerAddress + "api/hostIds/" + SharingSide_HubId_TextBox.Text);
+			}
+			catch (HttpRequestException) {
+				return;
+			}
+		}
 
 		private void ControlHost_Button_Click(object sender, RoutedEventArgs e) {
-			Window window = new ScreenControllingWindow(Settings, controlHost_HostId.Text, controlHost_Passcode.Text);
+			Window window = new ScreenControllingWindow(Settings, ControllingSide_HostId_TextBox.Text, ControllingSide_Passcode_TextBox.Text);
 			Visibility = Visibility.Hidden;
 			window.Show();
 			window.Closing += (sender, args) => {
@@ -33,7 +46,7 @@ namespace ScreenControlApp.Desktop {
 		}
 
 		private void ShareHost_Button_Click(object sender, RoutedEventArgs e) {
-			Window window = new ScreenSharingWindow(Settings, shareHost_HostId.Text, shareHost_Passcode.Text);
+			Window window = new ScreenSharingWindow(Settings, SharingSide_HubId_TextBox.Text, SharingSide_Passcode_TextBox.Text);
 			Visibility = Visibility.Hidden;
 			window.Show();
 			window.Closing += (sender, args) => {
@@ -41,8 +54,6 @@ namespace ScreenControlApp.Desktop {
 			};
 		}
 
-
-		private static bool IsSettingsPageDisplayed = false;
 		private void NavBar_SettingsButton_Click(object sender, RoutedEventArgs e) {
 			if (!IsSettingsPageDisplayed) {
 				SCA_Hub_Controls_Panel.Visibility = Visibility.Collapsed;//TODO:Rename panels to mainpanel
@@ -58,17 +69,54 @@ namespace ScreenControlApp.Desktop {
 			IsSettingsPageDisplayed = !IsSettingsPageDisplayed;
 		}
 
-		private void Window_Loaded(object sender, RoutedEventArgs e) {
-			SetUpDisplayInformation();
-			Loading_ProgressBar.Value += 50;
-			LoadSettings();
-			Loading_ProgressBar.Value += 25;
-			PopulateSettings();
-			Loading_ProgressBar.Value += 25;
+		private void Settings_Panel_SaveSettings_Button_Click(object sender, RoutedEventArgs e) {
+			Settings.ServerAddress = Settings_Panel_ServerAddress_TextBox.Text;
+			Settings.HubName = Settings_Panel_HubName_TextBox.Text;
+			Settings.PreferredScreenId = Settings_Panel_ScreenSelector_ComboBox.SelectedIndex;
 
-			SCA_Loading_Panel.Visibility = Visibility.Collapsed;
-			SCA_Bottom_Ribbon_Panel.Visibility = Visibility.Visible;
-			SCA_MainContent_Panel.Visibility = Visibility.Visible;
+			try {
+				var loader = new ApplicationSettingsLoader();
+				loader.Save(Settings, "settings.json");
+				Settings_Panel_SavedSettings_Label.Visibility = Visibility.Visible;
+			}
+			catch (Exception ex) {
+				System.Windows.MessageBox.Show(ex.Message);
+				CancellationTokenSource.Cancel();
+			}
+		}
+
+
+		#region MainWindow_LoadingOverlay
+		private void Window_Loaded(object sender, RoutedEventArgs e) {
+			_ = Task.Run(async () => {
+				this.Dispatcher.Invoke(() => {
+					SetUpDisplayInformation();
+					Loading_ProgressBar.Value += 25;
+				});
+
+				LoadSettings();
+				this.Dispatcher.Invoke(() => Loading_ProgressBar.Value += 25);
+
+				this.Dispatcher.Invoke(() => {
+					PopulateSettings();
+					Loading_ProgressBar.Value += 25;
+				});
+
+				await RequestHostId();
+				this.Dispatcher.Invoke(() => Loading_ProgressBar.Value += 25);
+
+				if (CancellationTokenSource.IsCancellationRequested)
+					System.Windows.Application.Current.Shutdown(); //TODO, does this even work?
+				else
+					this.Dispatcher.Invoke(() => {
+						SCA_Loading_Panel.Visibility = Visibility.Collapsed;
+						SCA_Bottom_Ribbon_Panel.Visibility = Visibility.Visible;
+						SCA_MainContent_Panel.Visibility = Visibility.Visible;
+					});
+			});
+		}
+		private void SetUpDisplayInformation() {
+			Settings_Panel_ScreenSelector_ComboBox.ItemsSource = DisplayInformation.GetMonitorsInfo();
 		}
 
 		private void LoadSettings() {
@@ -80,8 +128,10 @@ namespace ScreenControlApp.Desktop {
 				System.Windows.MessageBox.Show(ex.Message);
 			}
 		}
+
 		private void PopulateSettings() {
 			Settings_Panel_ServerAddress_TextBox.Text = Settings.ServerAddress;
+			Settings_Panel_HubName_TextBox.Text = Settings.HubName;
 			if (Screen.AllScreens.Length - 1 < Settings.PreferredScreenId) {
 				var screenSelections = (List<string>)Settings_Panel_ScreenSelector_ComboBox.ItemsSource;
 				int index = screenSelections.FindIndex(screenSelection => screenSelection.Contains("Primary"));
@@ -91,22 +141,21 @@ namespace ScreenControlApp.Desktop {
 				Settings_Panel_ScreenSelector_ComboBox.SelectedIndex = Settings.PreferredScreenId;
 		}
 
-		private void SetUpDisplayInformation() {
-			Settings_Panel_ScreenSelector_ComboBox.ItemsSource = DisplayInformation.GetMonitorsInfo();
-		}
-
-		private void Settings_Panel_SaveSettings_Button_Click(object sender, RoutedEventArgs e) {
-			Settings.ServerAddress = Settings_Panel_ServerAddress_TextBox.Text;
-			Settings.PreferredScreenId = Settings_Panel_ScreenSelector_ComboBox.SelectedIndex;
-
+		private async Task RequestHostId() {
+			using HttpClient client = new();
 			try {
-				var loader = new ApplicationSettingsLoader();
-				loader.Save(Settings, "settings.json");
-				Settings_Panel_SavedSettings_Label.Visibility = Visibility.Visible;
+				HttpResponseMessage response = await client.GetAsync(Settings.ServerAddress + "api/hostIds");
+				response.EnsureSuccessStatusCode();
+				string generatedHostId = await response.Content.ReadAsStringAsync();
+				this.Dispatcher.Invoke(() => SharingSide_HubId_TextBox.Text = generatedHostId);
 			}
-			catch (Exception ex) {
-				System.Windows.MessageBox.Show(ex.Message);
+			catch (HttpRequestException e) {
+				System.Windows.MessageBox.Show("Error while connecting to the server: " + e.ToString());
+				CancellationTokenSource.Cancel();
 			}
 		}
+		#endregion
+
+
 	}
 }
