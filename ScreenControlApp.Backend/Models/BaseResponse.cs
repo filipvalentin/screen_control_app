d06ed4db896bdef1b87e132b524caa1dc516d637
filipvalentin <valentin.filip@student.uaic.ro>
2024-09-09 10:41:34 +0300

    -

---
+++ ScreenControlApp.Backend/Models/BaseResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace ScreenControlApp.Backend.Models {
+	public class BaseResponse {		
+		public bool IsSuccess { get; set; } = default!;
+		public string Message { get; set; } = default!;
+		public BaseResponse() => IsSuccess = true;
+		//public BaseResponse(string message, bool success) {
+		//	IsSuccess = success;
+		//	Message = message;
+		//}
+	}
+}
